0d4ae790d6c617532d3cdca4dbe87d25424b79eb
Maciej Maciejewski <maciejewskym@gmail.com>
2020-05-02 14:50:36 +0200

    create new spring boot project

---
+++ src/test/java/msky/personalbudget/PersonalBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package msky.personalbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
